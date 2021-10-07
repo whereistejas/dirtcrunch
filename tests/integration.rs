97ad300f7689a7e0670f6ee4f41978c0ed90b128
Tejas Sanap <sanap.tejas@gmail.com>
2021-10-08 01:40:56 +0530

    Fix test to use new methods.

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,34 +1,9 @@
-use dirtcrunch::*;
-use serde_json::Value;
-
-// This test is basically an example of how to use this crate.
-
-struct File {}
-
-impl File {
-    pub fn new() -> Self {
-        Self {}
-    }
-}
-
-impl Source for File {
-    const CONNECTOR: &'static str = "airbyte/source-file";
-}
+use dirtcrunch::{create_file, get_specs};
 
 #[tokio::test]
 async fn test() {
-    let file = File::new();
+    let json = get_specs("airbyte/source-file").await;
+    let file = create_file("SourceFile", json);
 
-    let result = file.specs().await;
-
-    let result: Value = serde_json::from_str(result.as_str()).unwrap();
-    let result = result
-        .get("spec")
-        .unwrap()
-        .get("connectionSpecification")
-        .unwrap()
-        .get("required")
-        .unwrap();
-
-    assert_eq!(result.as_array().unwrap().len(), 4);
+    println!("{:#?}", file);
 }
